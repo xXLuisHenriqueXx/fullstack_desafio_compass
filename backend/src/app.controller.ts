634c0c8da964233196f933d7ca8838a75a68afda
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-05-13 21:05:11 -0300

    ADD: swagger for health routes

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,15 +1,22 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { ApiOperation } from "@nestjs/swagger";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({
+    summary: "Return the health state of api",
+  })
   @Get("health/api")
   checkApi(): { status: string } {
     return this.appService.checkApiHealth();
   }
 
+  @ApiOperation({
+    summary: "Return the health state of database",
+  })
   @Get("health/db")
   async checkDb(): Promise<{ status: string }> {
     return this.appService.checkDatabaseHealth();
